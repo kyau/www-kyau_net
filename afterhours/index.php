f6cf2274f4f8b53dc0bf2f2a188ed07b3c4739ce
kyau <kyau@kyau.net>
2016-02-04 12:26:15 -0500

    Small Update * AfterHours stuff updated for 1.8.9 modpack * Random fixes all around

--- afterhours/index.php
+++ afterhours/index.php
@@ -5,10 +5,22 @@
     <title>AfterHours</title>
     <link href="//fonts.googleapis.com/css?family=Electrolize:400" rel="stylesheet" />
     <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" />
+    <link href="http://fonts.googleapis.com/css?family=Orbitron:500" rel="stylesheet" type="text/css">
     <link href="css/screen.css" rel="stylesheet" />
     <link href="/img/fugue-icons/icons/controller.png" rel="icon" />
-    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/jquery.mousewheel-3.1.12.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
     <script src="js/main.js" charset="utf-8"></script>
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="js/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="js/jquery.fancybox.pack.js?v=2.1.5"></script>
+    <link rel="stylesheet" href="js/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="js/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="js/jquery.fancybox-media.js?v=1.0.6"></script>
+    <link rel="stylesheet" href="js/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
   </head>
 
   <body>
@@ -16,36 +28,34 @@
     <div id="bg">
       <div id="toolbar">
       <div class="minecraft circle">
-        <img alt="AfterHours" src="/img/steve.png" /><span>Minecraft</span>
+        <img alt="AfterHours" src="/img/minecraft.png" /><br/>
         <p></p>
-        <p style="font-size:10pt;line-height:12pt;">AfterHours is a white-listed modded minecraft server similar to FTB or Tekkit.</p>
+        <p style="font-size:10pt;line-height:12pt;">AfterHours is a hard-mode progression based modpack for minecraft. It has recently been updated for 1.8.9.</p>
         <a class="lnk" href="http://kyau.net/files/">download</a><br />
-        <a class="lnk" href="https://github.com/kyau/afterhours">modpack</a><br />
-        <a class="lnk" id="a_rules" href="#">rules</a><br />
+        <a class="lnk" id="a_rules">rules</a><br />
       </div>
       <div class="games circle">
-        <img alt="Games" src="/img/games.png" /><span>Games</span>
+        <img alt="Games" src="/img/games.png" /><span>Server Info</span>
         <p></p>
-        <a class="lnk" href="http://steamcommunity.com/groups/afterhours-gaming">Counter-Strike: GO</a><br />
-        <a class="lnk" href="http://steamcommunity.com/groups/afterhours-gaming">Path of Exile</a><br />
-        <a class="lnk" href="#">Star Citizen</a><br />
-        <a class="lnk" href="http://steamcommunity.com/groups/afterhours-gaming">Starbound</a><br />
+        <p style="font-size:10pt;line-height:12pt;">If you are interested in joining AfterHours contact me to get white-listed.</p>
+        <a class="lnk" href="http://steamcommunity.com/id/kyauorg">steamid</a><br />
+        <a class="lnk" href="irc://irc.esper.net:+6697/minecraftforge">#minecraftforge/kyau</a><br />
       </div>
       <div class="status circle">
-        <img alt="Status" src="/img/status.png" /><span>Status</span>
+        <img alt="Status" src="/img/status.png" /><br/>
         <p></p>
         <?php
           // Minecraft status
           $connection = @fsockopen('dark.kyau.net', 25565);
           if ($connection) {
-            echo 'Minecraft Server<br /><a href="#">
+            echo 'Forge Server<br /><a>
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="69" height="14">
           <rect x="7" y="5" height="6" width="6" rx="2" ry="2" fill="rgba(93,206,51,1)" />
           <text dy=".75em" x="22" y="0" fill="rgba(216,216,216,1)">online</text>
         </svg></a><br />';
             fclose($connection);
           } else {
-            echo 'Minecraft Server<br /><a href="#">
+            echo 'Forge Server<br /><a>
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="71" height="14">
           <rect x="7" y="5" height="6" width="6" rx="2" ry="2" fill="rgba(190,25,25,1)" />
           <text dy=".75em" x="22" y="0" fill="rgba(216,216,216,1)">offline</text>
@@ -71,22 +81,40 @@
         ?>
       </div>
       </div>
-      <div id="logo"><img alt="AfterHours Logo" src="/img/logo-afterhours.png" /></div>
+      <div id="logo">
+        <img class="logo" alt="AfterHours Logo" src="/img/logo-afterhours.png" />
+        <div id="final-countdown">&nbsp;</div>
+        <!--<script type="text/javascript">
+          $("#final-countdown")
+          .countdown("2015/02/11", function(event) {
+            $(this).html(
+              event.strftime('<div><span class="time">%w<br/><br/><span class="dur">weeks</span></span></div> <div><span class="time">%d<br/><br/><span class="dur">days</span></span></div> <div><span class="time">%H<br/><br/><span class="dur">hours</span></span></div> <div><span class="time">%M<br/><br/><span class="dur">min</span></span></div> <div><span class="time">%S<br/><br/><span class="dur">sec</span></span></div>')
+            );
+          });
+        </script>-->
+        <br/>
+        <div id="sneak-peak">
+          <a class="fancybox" rel="fancybox" href="/afterhours/sneak-peak/afterhours-1.png" title="Newly added first time load screen."><img src="/afterhours/sneak-peak/afterhours-1-s.png" alt="" /></a>
+          <a class="fancybox" rel="fancybox" href="/afterhours/sneak-peak/afterhours-2.png" title="The redesigned main menu."><img src="/afterhours/sneak-peak/afterhours-2-s.png" alt="" /></a>
+          <a class="fancybox" rel="fancybox" href="/afterhours/sneak-peak/afterhours-3.png" title="The amazing brand new GUI with BetterFonts integration!"><img src="/afterhours/sneak-peak/afterhours-3-s.png" alt="" /></a>
+          <a class="fancybox" rel="fancybox" href="/afterhours/sneak-peak/afterhours-4.png" title="AfterHours modpack support mod adds useful items and the necessary pack textures."><img src="/afterhours/sneak-peak/afterhours-4-s.png" alt="" /></a>
+        </div>
+      </div>
       <div id="maintenance">
         <h1>Server Maintenance</h1>
         The server is currently undergoing maintenance, please check back later!
       </div>
       <div id="rules">
         <div id="rules_scroll">
-        <h1>Minecraft Server Rules</h1>
+        <h1>Server Rules</h1>
         <p><span>Mindful Thoughts...</span><br /><list>
-        AfterHours is a place for mature adults (18+) to express their architectural
-        and engineering dreams. We hope that we can provide all users with the
-        perfect environment with which to foster and encourage growth as a
-        Minecraft architect or engineer. Whether it is merely providing the
-        canvas, introducing you to your new partner in crime or just providing
-        some light conversation while you take over the universe with your
-        inter-planetary empire, we hope that you will enjoy your stay at AfterHours.</list></p>
+        AfterHours is a place for mature adults (18+) to express their architectural and
+        engineering dreams. We hope that we can provide all users with the perfect
+        environment with which to foster and encourage growth as a Minecraft architect or
+        engineer. Whether it is merely providing the canvas, introducing you to your new
+        partner in crime or just providing some light conversation while you take over the
+        universe with your inter-dimensional empire, we hope that you will enjoy your stay
+        at AfterHours.</list></p>
         <p><span>Bannable Offenses</span><br /><list>
         &#xffed; Cheating, Exploits, Griefing, Stealing & Xray<br />
         &#xffed; Building of anything <a href="http://en.wikipedia.org/wiki/Political_correctness">politically incorrect</a><br />
@@ -95,19 +123,10 @@
         &#xffed; Advertising or promoting other servers/communities</list></p>
         <p><span>Building Specs</span><br /><list>
         &#xffed; 256 blocks: minimum build distance to not require permission from your neighboors<br />
-        &#xffed; Try to make everything you build aesthetically pleasing, we understand
-        not everyone is an artist merely try your best<br />
-        &#xffed; All major/automated mining operations are to be done in Mining World Alpha/Beta<br />
-        &#xffed; Random ages will be created dependent on server progression<br />
-        <p><span>Banned Items</span><br /><list>
-        Industrialcraft2 &#151; Nuke<br />
-        Mystcraft &#151; Book Binder (OP only)<br />
-        OpenBlocks &#151; Build Helper<br />
-        Railcraft &#151; Anchor Cart, Primer Track, TNT Cart, World Anchor</list></p>
+        &#xffed; Try to make everything you build aesthetically pleasing, we understand not
+        everyone is an artist merely try your best<br />
         </div>
         <div class="close">X</div>
-        <img class="up" src="/img/fugue-icons/icons/arrow-090.png" />
-        <img class="down" src="/img/fugue-icons/icons/arrow-270.png" />
       </div>
       <div id="map"><a href="//dark.kyau.net:8123/" title="Live Map!"><img src="/img/minecraft-24.png" /></a></div>
       <div id="plugins">
@@ -128,16 +147,22 @@
         if(($info = $query->GetInfo()) !== false) {
           $software = split(' ', $info['Software']);
           $version = shell_exec("cat ~/bin/minecraft | grep '^VERSION=' | sed -e \"s/VERSION\='//\" -e \"s/'//\"");
-          $mcpc = $software[0].' '.$version;
-          $plugins = $info['Plugins'];
-          asort($plugins, SORT_NATURAL | SORT_FLAG_CASE);
-          foreach($plugins as &$plugin) {
-            $tmp = explode(' ',trim($plugin));
-            if(!($tmp[0] == 'EssentialsChat' || $tmp[0] == 'EssentialsSpawn' || $tmp[0] == 'Creeper')) {
-              $tempver = split('-', $tmp[1]);
-              echo '&#xffed; '.$tmp[0].' '.$tempver[0].'<br />';
+          if($software[0] == 'Vanilla') {
+            $mcpc = 'Forge '.$version;
+            echo '<span style="color:#a020f0;font-weight:bold;">Minecraft 1.8.9</span><br/>&#xffed; AfterHours-0.23-11<br/>&#xffed; CompassCoordinates-0.2.6<br/>&#xffed; HelpFixer-1.2.12<br/>&#xffed; JourneyMap-5.1.3<br/>&#xffed; Morpheus-2.0.57';
+          } else {
+            $mcpc = $software[0].' '.$version;
+            $plugins = $info['Plugins'];
+            asort($plugins, SORT_NATURAL | SORT_FLAG_CASE);
+            foreach($plugins as &$plugin) {
+              $tmp = explode(' ',trim($plugin));
+              if(!($tmp[0] == 'EssentialsChat' || $tmp[0] == 'EssentialsSpawn' || $tmp[0] == 'Creeper')) {
+                $tempver = split('-', $tmp[1]);
+                echo '&#xffed; '.$tmp[0].' '.$tempver[0].'<br />';
+              }
             }
           }
+
       ?>
       </div>
       <div id="server1">
@@ -150,7 +175,7 @@
           if(($players = $query->GetPlayers()) !== false):
             foreach($players as $player):
               $player = htmlspecialchars($player);
-              echo ' <img src="https://minotar.net/avatar/'.$player.'/22.png" title="'.$player.'" />';
+              echo ' <img src="http://minotar.net/avatar/'.$player.'/22.png" title="'.$player.'" />';
             endforeach;
             echo '<br />';
           endif;
@@ -168,6 +193,21 @@
         }
       ?>
     </div>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $(".fancybox").fancybox({
+          helpers : {
+            closeBtn : false,
+            title : { type : 'inside' },
+            buttons : {},
+            thumbs : {
+              width: 128,
+              height: 72
+            }
+          }
+        });
+      });
+    </script>
   </body>
 </html>
 <!--
--- index.php
+++ index.php
@@ -1,59 +1,60 @@
+<?php
+echo "
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
   <head>
-    <meta charset="utf-8" />
+    <meta charset='utf-8' />
     <title>/home/kyau</title>
-    <link href="http://fonts.googleapis.com/css?family=Electrolize:400" rel="stylesheet" />
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" />
-    <link href="css/screen.css" rel="stylesheet" />
-    <link href="/img/fugue-icons/icons/home.png" rel="icon" />
-    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/main.js" charset="utf-8"></script>
+    <link href='http://fonts.googleapis.com/css?family=Electrolize:400' rel='stylesheet' />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' />
+    <link href='css/screen.css' rel='stylesheet' />
+    <link href='/img/fugue-icons/icons/home.png' rel='icon' />
   </head>
 
   <body>
-    <?php include_once("php/analytics.php") ?>
-    <div id="bg">
-      <div id="toolbar">
-      <div class="afterhours circle">
-        <img alt="AfterHours" src="img/afterhours.png" /><span>AfterHours</span>
-        <p style="font-size:10pt;line-height:12pt;">AfterHours is a gaming community founded in 2010 originating from an MMORPG guild. With the release of Minecraft all of that changed, since then we have grown into a multi-game community.</p>
-        <span class="cyan">&rsaquo;</span> <a href="http://kyau.net/afterhours/">visit site</a> <span class="cyan">&lsaquo;</span>
+    <?php include_once('php/analytics.php') ?>
+    <div id='bg'>
+      <div id='toolbar'>
+      <div class='fpv circle'>
+        <img alt='FPV' src='img/afterhours.png' /><span>AfterHours</span>
+        <p style='font-size:10pt;line-height:12pt;'>A Custom Modpack for Minecraft v1.8.9 designed for casual and hardcore players alike.</p>
+        <span class='cyan'>&rsaquo;</span> <a href='http://kyau.net/afterhours/'>visit site</a> <span class='cyan'>&lsaquo;</span>
       </div>
-      <div class="stuff circle">
-        <img alt="My Stuff" src="img/stuff.png" /><span>My Stuff</span>
+      <div class='stuff circle'>
+        <img alt='My Stuff' src='img/stuff.png' /><span>Links</span>
         <p></p>
-        <a class="lnk" href="https://www.dropbox.com/sh/8jxy9hxzhdfng8j/OUHX6IgICi">dropbox</a><br />
-        <a class="lnk" href="https://gist.github.com/kyau">gist</a><br />
-        <a class="lnk" href="https://github.com/kyau">github</a><br />
-        <a class="lnk" href="http://kyau.net/wiki/">wiki</a><br />
+        <a class='lnk' href='https://gist.github.com/kyau'>Gist</a><br />
+        <a class='lnk' href='https://github.com/kyau'>Github</a><br />
+        <a class='lnk' href='https://www.instagram.com/thekyau/'>Instagram</a><br />
+        <a class='lnk' href='https://www.youtube.com/channel/UCya_qUkryt4ftq-MsdFuslA'>YouTube</a><br />
+        <a class='lnk' href='http://kyau.net/wiki/'>Wiki</a><br />
       </div>
-      <div class="projects circle">
-        <img alt="Projects" src="img/projects.png" /><span>Projects</span>
+      <div class='projects circle'>
+        <img alt='Projects' src='img/projects.png' /><span>Projects</span>
         <p></p>
-
+        <a class='lnk' href='http://bbs.kyau.net/'>BBS</a><br />
+        <a class='lnk' href='/d2'>Diablo 2</a><br />
+        <a class='lnk' href='http://unity.kyau.net/'>Unity3d</a><br />
+        <a class='lnk' href='/zmr250/'>ZMR250</a><br />
       </div>
-      <div class="server circle">
-        <img alt="Server" src="img/server.png" /><span>Server</span>
+      <div class='server circle'>
+        <img alt='Server' src='img/server.png' /><span>&nbsp;</span>
         <p></p>
-        <a class="lnk" href="http://dark.kyau.net/">file manager</a><br />
-        <a class="lnk" href="http://dark.kyau.net:9091/transmission/web/">transmission</a><br />
-        <a class="lnk" href="https://dark.kyau.net:6697">znc</a><br />
+        <a class='lnk' href='http://dark.kyau.net/'>file manager</a><br />
       </div>
       </div>
-      <div id="logo"><img alt="AfterHours Logo" src="img/logo.png" /></div>
-      <div id="info"></div>
-      <div id="version">
-      <?php
-        exec('git rev-parse --short HEAD', $output);
-        print $_SERVER['SERVER_SOFTWARE']." PHP/".phpversion().
-          ' github/<a href="https://github.com/kyau/www-kyau_net">0.3-'.
-          $output[0]."</a>";
-      ?>
+      <div id='logo'><img alt='AfterHours Logo' src='img/logo.png' /></div>
+      <div id='info'></div>
+      <div id='version'>";
+  exec("git rev-parse --short HEAD", $output);
+  print $_SERVER['SERVER_SOFTWARE']." PHP/".phpversion()." github/<a href='https://github.com/kyau/www-kyau_net'>0.3-".$output[0]."</a>";
+  echo "
       </div>
     </div>
+
+    <script src='//code.jquery.com/jquery-1.11.3.min.js'></script>
+    <script src='js/main.js' charset='utf-8'></script>
   </body>
-</html>
-<!--
-  vim: ts=2 sw=2
--->
+</html>";
+
+?>
